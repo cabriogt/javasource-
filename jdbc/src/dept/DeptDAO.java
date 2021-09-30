545d388c89b0d23aa64f1215e3e20711eef23f1f
Juan <cabriogt@gmail.com>
2021-09-30 15:59:10 +0900

    "database coneccion

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -2,9 +2,19 @@ package dept;
 
 public class DeptDAO {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+			
 	}
-
+	public static Connection getConnection () {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user= "C##scott";
+		String password ="tiger";
+		
+		
+	}
 }
