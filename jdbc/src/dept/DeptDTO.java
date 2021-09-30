5ffacdbdcae773f259835dd6299e6e64396b9000
Juan <cabriogt@gmail.com>
2021-09-30 14:40:07 +0900

    depto project create

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,6 +1,24 @@
 package dept;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 public class DeptDTO {
+	
+	
+	private int deptNO;
+	private String dname;
+	private String loc;
+	
+	
 
 
 	
